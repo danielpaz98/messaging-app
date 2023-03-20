d18fbe56041d1f3d1c21db755b1a224b06c4cf8b
danielpaz98 <alexdaniel-1998@outlook.com>
2023-03-20 16:00:14 -0500

    feat: add Sidebar component

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,67 @@
+// PLUGINS
+import clsx from "clsx";
+// IMAGES
+import { ReactComponent as Logo } from "~/assets/images/svg/logo.svg";
+// ICONS
+import { ReactComponent as Grid2Icon } from "~/assets/icons/grid-2.svg";
+import { ReactComponent as FilterIcon } from "~/assets/icons/filter.svg";
+import { ReactComponent as BookOpenIcon } from "~/assets/icons/book-open.svg";
+import { ReactComponent as GearIcon } from "~/assets/icons/gear.svg";
+import { ReactComponent as PaperPlaneTopIcon } from "~/assets/icons/paper-plane-top.svg";
+import { ReactComponent as FolderPlusIcon } from "~/assets/icons/folder-plus.svg";
+
+type Props = {
+	className?: string;
+};
+
+export default function Sidebar({ className }: Props) {
+	const classNames = clsx(className);
+
+	return (
+		<nav className="bg-[#1e1f25] min-w-[94px] min-h-screen px-4 py-5">
+			<a href="/">
+				<Logo height={64} width={64} />
+			</a>
+
+			<section className="mt-[150px]">
+				<ul className="grid place-items-center gap-11">
+					<li>
+						<button type="button">
+							<Grid2Icon className="pointer-events-none" fill="#5f6388" height={20} width={20} />
+						</button>
+					</li>
+
+					<li>
+						<button type="button">
+							<FilterIcon className="pointer-events-none" fill="#5f6388" height={20} width={20} />
+						</button>
+					</li>
+
+					<li>
+						<button type="button">
+							<BookOpenIcon className="pointer-events-none" fill="#5f6388" height={20} width={20} />
+						</button>
+					</li>
+
+					<li>
+						<button type="button">
+							<GearIcon className="pointer-events-none" fill="#5f6388" height={20} width={20} />
+						</button>
+					</li>
+
+					<li>
+						<button className="bg-[#5051f9] p-3 rounded-[14px] shadow-[0_8px_14px_rgba(62,107,224,0.12)]" type="button">
+							<PaperPlaneTopIcon className="pointer-events-none" fill="#fff" height={20} width={20} />
+						</button>
+					</li>
+
+					<li>
+						<button type="button">
+							<FolderPlusIcon className="pointer-events-none" fill="#5f6388" height={20} width={20} />
+						</button>
+					</li>
+				</ul>
+			</section>
+		</nav>
+	);
+}
