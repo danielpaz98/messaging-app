06bb45fc9975120c14584051bb1428f4c594c615
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-03-30 14:48:57 -0500

    add: DropdownUserMenu component

--- src/components/TopBar/DropdownUserMenu/index.tsx
+++ src/components/TopBar/DropdownUserMenu/index.tsx
@@ -1,5 +1,7 @@
 // PLUGINS
-import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+// COMPONENTS
+import ProfilePicture from "~/components/ProfilePicture";
 // ICONS
 import { ReactComponent as ArrowDownIcon } from "~/assets/icons/arrow-down.svg";
 
@@ -8,17 +10,11 @@ type Props = {
 };
 
 export default function DropdownUserMenu({ className }: Props) {
-	const classNames = clsx("cursor-pointer flex items-center gap-3", className);
+	const classNames = twMerge("cursor-pointer flex items-center gap-3", className);
 
 	return (
 		<div className={classNames}>
-			<picture className="block w-10 h-10 bg-[#e7e7ff] overflow-hidden rounded-full">
-				<img
-					alt="Profile Avatar"
-					className="object-contain"
-					src="https://api.dicebear.com/5.x/personas/svg?seed=Jack"
-				/>
-			</picture>
+			<ProfilePicture src="https://api.dicebear.com/5.x/personas/svg?seed=Jack&backgroundColor=e7e7ff" />
 
 			<i>
 				<ArrowDownIcon className="pointer-events-none" fill="#fefefe" height={14} width={14} />
