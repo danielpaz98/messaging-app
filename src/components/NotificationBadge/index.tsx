c9f99525776e3bdb5363c87f57483c907fe105fa
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-03-30 14:46:26 -0500

    add: NotificationBadge component

---
+++ src/components/NotificationBadge/index.tsx
@@ -0,0 +1,16 @@
+// PLUGINS
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+	className?: string;
+	notifications: number;
+};
+
+export default function NotificationBadge({ className, notifications }: Props) {
+	const classNames = twMerge(
+		"grid place-content-center text-[8px] font-bold bg-[#000] text-[#fff] rounded-full w-[12px] h-[12px]",
+		className
+	);
+
+	return <span className={classNames}>{notifications}</span>;
+}
