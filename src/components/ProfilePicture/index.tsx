39b6ff3a36110bfa48a5cfb9a0bafc7b42acf7d7
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-03-30 14:46:40 -0500

    add: ProfilePicture component

---
+++ src/components/ProfilePicture/index.tsx
@@ -0,0 +1,17 @@
+// PLUGINS
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+	className?: string;
+	src: string;
+};
+
+export default function ProfilePicture({ className, src }: Props) {
+	const classNames = twMerge("block w-10 h-10 rounded-full", className);
+
+	return (
+		<picture className={classNames}>
+			<img alt="Profile Avatar" className="object-contain rounded-[inherit]" src={src} />
+		</picture>
+	);
+}
