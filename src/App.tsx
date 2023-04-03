f79daa7ade12e6e65e1f728a4423ce30fa5a1813
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2023-04-03 17:57:19 -0500

    feat: use ChatInfo component

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ export default function App() {
 	return (
 		<DefaultLayout>
 			<ChatView className="h-full grid grid-cols-12 auto-rows-fr">
-				<ChatView.ChatList className="col-span-4" />
-				<ChatView.Chat className="col-span-8" />
+				<ChatView.ChatList className="col-span-3" />
+				<ChatView.Chat className="col-span-6" />
+				<ChatView.ChatInfo className="col-span-3" />
 			</ChatView>
 		</DefaultLayout>
 	);
